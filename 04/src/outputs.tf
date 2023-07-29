fa704c0ad6d43bb7be25188faba088f65dc1c3b6
EVolgina <123949683+EVolgina@users.noreply.github.com>
2023-07-29 20:17:35 +0500

    Update outputs.tf

--- 04/src/outputs.tf
+++ 04/src/outputs.tf
@@ -1,6 +1,11 @@
 output "subnet" {
-value = yandex_vpc_subnet.subnet.test2
+  value = yandex_vpc_subnet.subnet
 }
-output "subnet_id" {
-  value = yandex_vpc_subnet.subnet.id
+
+output "vpc_name" {
+  value = yandex_vpc_network.vpc.name
+}
+
+output "network_id" {
+  value = yandex_vpc_network.vpc.id
 }
